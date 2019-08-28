3825951c3dca84e111206dc67a031b9c2124a7b9
Igor Sushko <igor_sushko@yahoo.com>
2019-08-28 14:29:22 +0500

    Два разных независимых dropdown

--- src/UI-kit/library/blocks/01-elements/dropdown/dropdown-start-script.js
+++ src/UI-kit/library/blocks/01-elements/dropdown/dropdown-start-script.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-  $('.iqdropdown').iqDropdown({ 
+  $('.first-iqdropdown').iqDropdown({ 
 		maxItems: Infinity,
 		// min total items
 		minItems: 1,
@@ -11,6 +11,35 @@ $(document).ready(() => {
 			['Спальня', 'Спальни','Спален'],
 			['Кровать', 'Кровати','Кроватей'],
 			['Ванная комната', 'Ванные комнаты', 'Ванных комнат']],
+		numericLimitArrayForPlural: [0, 2, 5]
+		// buttons to increment/decrement
+		// controls: {
+		// 	position: 'right',
+		// 	displayCls: 'iqdropdown-item-display',
+		// 	controlsCls: 'iqdropdown-item-controls',
+		// 	counterCls: 'counter'
+		// },
+		// fires when an item quantity changes
+		// onChange: (id, count, totalItems) => {},
+		// return false to prevent an item decrement
+		// beforeDecrement: (id, itemCount) => {},
+		// return false to prevent an item increment
+		// beforeIncrement: (id, itemCount) => {}
+	});
+});
+$(document).ready(() => {
+  $('.second-iqdropdown').iqDropdown({ 
+		maxItems: Infinity,
+		// min total items
+		minItems: 1,
+		// text to show on the dropdown
+		selectionText: 'item',
+		// text to show for multiple items
+		textPlural: 'items',
+		textArrayForPlural: [
+			['Взрослый', 'Взрослых','Взрослых'],
+			['Ребонок', 'Ребенка','Ребят'],
+			['Младенец', 'Младенца', 'Младенцев']],
 		numericLimitArrayForPlural: [0, 2, 5]
 		// buttons to increment/decrement
 		// controls: {
