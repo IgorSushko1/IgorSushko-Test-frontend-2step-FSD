9ddab43f4f2472728c982c95cee0fb48ad55c1c9
Igor Sushko <igor_sushko@yahoo.com>
2019-09-05 12:50:48 +0500

    footers and heders done!

--- webpack.tryscss.js
+++ webpack.tryscss.js
@@ -56,10 +56,10 @@ module.exports = {
 				test: /\.(svg|png|jpg|gif)$/,
 				use: {
 					loader: "file-loader",
-					// options: {
-					// name: "[name].[ext]",
-					// outputPath: "imgs"
-					// }
+					options: {
+					name: "[name].[ext]",
+					outputPath: "imgs"
+					}
 				}
 			}
 		],
