a96c00c6703edb3c5c96a454b6993eaf1c40a92b
Igor Sushko <igor_sushko@yahoo.com>
2019-12-11 00:39:28 +0500

    Наименования файлов, классов по БЭМ #7
    Частично. Изменения внес в часть элементов и страниц.
    
    Блоки и 8 элементов не исправлены.

--- src/components/elements/checkbox-expendable/checkbox-expendable.js
+++ src/components/elements/checkbox-expendable/checkbox-expendable.js
@@ -1,13 +1,13 @@
 function foo(){
-	var elem = document.querySelectorAll(".dropdownCheckboxes")[0];
-	elem.classList.toggle("dropdown-show");
-	if (document.querySelectorAll(".dropdown-btn-icons")[0]) {
-		var icon = document.querySelectorAll(".dropdown-btn-icons")[0];
+	var elem = document.querySelectorAll(".checkbox-expendable__dropdown")[0];
+	elem.classList.toggle("checkbox-expendable__dropdown_show");
+	if (document.querySelectorAll(".checkbox-expendable__icons")[0]) {
+		var icon = document.querySelectorAll(".checkbox-expendable__icons")[0];
 		if (icon.innerHTML == "keyboard_arrow_up") {icon.innerHTML = "keyboard_arrow_down";}
 		else {icon.innerHTML = "keyboard_arrow_up";}
 	}
 
 }
-if (document.querySelectorAll(".myDropdown007")[0]){
-	document.querySelectorAll(".myDropdown007")[0].addEventListener("click", foo);
+if (document.querySelectorAll(".checkbox-expendable__start")[0]){
+	document.querySelectorAll(".checkbox-expendable__start")[0].addEventListener("click", foo);
 }
--- src/components/elements/checkbox-toggle/jquery.checkbox-toggle.js
+++ src/components/elements/checkbox-toggle/jquery.checkbox-toggle.js
@@ -1,47 +1,15 @@
-$("input.toggle-pets").click(function() {
-	if ($("input.toggle-pets").is(":checked")) {
-		$("#checkbox-background-gradient-pets").css("background","linear-gradient(180deg, #BC9CFF, #8BA4F9)");
-		$(".checkbox-toggle-pets").css({
-			"left": "22px",
-			"background": "linear-gradient(180deg, #BC9CFF, #8BA4F9)"
-		})
-	} else {
-		$(".checkbox-background-gradient-pets").css("background","linear-gradient(180deg, rgba(31, 32, 65, 0.25), rgba(31, 32, 65, 0.25)");
-		$(".checkbox-toggle-pets").css({
+$(".checkbox-toggle__text").click(function () {
+	if ($(".checkbox-toggle__input").is(":checked")) {
+		$(".checkbox-toggle__background_gradient").css("background", "linear-gradient(180deg, rgba(31, 32, 65, 0.25), rgba(31, 32, 65, 0.25)");
+		$(".checkbox-toggle__circle").css({
 			"left": "4px",
 			"background": "linear-gradient(180deg, rgba(31, 32, 65, 0.25), rgba(31, 32, 65, 0.25)"
 		})
-	};
-})
-
-$("input.toggle-smoke").click(function() {
-	if ($("input.toggle-smoke").is(":checked")) {
-		$(".checkbox-background-gradient-smoke").css("background","linear-gradient(180deg, #BC9CFF, #8BA4F9)");
-		$(".checkbox-toggle-smoke").css({
+	} else {
+		$(".checkbox-toggle__background_gradient").css("background", "linear-gradient(180deg, #BC9CFF, #8BA4F9)");
+		$(".checkbox-toggle__circle").css({
 			"left": "22px",
 			"background": "linear-gradient(180deg, #BC9CFF, #8BA4F9)"
-		})
-	} else {
-		$(".checkbox-background-gradient-smoke").css("background","linear-gradient(180deg, rgba(31, 32, 65, 0.25), rgba(31, 32, 65, 0.25)");
-		$(".checkbox-toggle-smoke").css({
-			"left": "4px",
-			"background": "linear-gradient(180deg, rgba(31, 32, 65, 0.25), rgba(31, 32, 65, 0.25)"
-		})
-	};
-})
-
-$("input.toggle-guests").click(function() {
-	if ($("input.toggle-guests").is(":checked")) {
-		$(".checkbox-background-gradient-guests").css("background","linear-gradient(180deg, #BC9CFF, #8BA4F9)");
-		$(".checkbox-toggle-guests").css({
-			"left": "22px",
-			"background": "linear-gradient(180deg, #BC9CFF, #8BA4F9)"
-		})
-	} else {
-		$(".checkbox-background-gradient-guests").css("background","linear-gradient(180deg, rgba(31, 32, 65, 0.25), rgba(31, 32, 65, 0.25)");
-		$(".checkbox-toggle-guests").css({
-			"left": "4px",
-			"background": "linear-gradient(180deg, rgba(31, 32, 65, 0.25), rgba(31, 32, 65, 0.25)"
 		})
 	};
 })
