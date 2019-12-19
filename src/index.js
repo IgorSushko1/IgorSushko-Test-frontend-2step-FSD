3ce4f73e49049ef26dbcbf4a7e88aa501b2d071a
Igor Sushko <igor_sushko@yahoo.com>
2019-12-19 14:01:46 +0500

    Добавил переход в соцсети

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import './style.scss';
 
-require('./components/elements/checkbox-toggle/jquery.checkbox-toggle');
+function requireJS(r) {
+  return r.keys().map(r);
+}
 
-require('./vendor/calendar/calendar');
-require('./vendor/calendar/datepicker');
-
-require('./vendor/range-slider/ion.rangeSlider');
-require('./vendor/range-slider/jquery.range-slider');
-
-require('./components/elements/checkbox-expendable/checkbox-expendable');
-
-require('./vendor/dropdown/jquery.dropdown-subparagraph-bedroom');
-require('./vendor/dropdown/dropdown-start-script');
+requireJS(require.context('./', true, /\.js$/));
